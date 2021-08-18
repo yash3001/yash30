00b39899ac0ea548dcbd86602174a5fb8411c401
Yash Chauhan <yashchauhan197@gmail.com>
2021-08-18 13:42:21 +0530

    save some animation to text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,28 @@
 import "./about.scss";
 import { init } from "ityped";
 import { useEffect, useRef } from "react";
+import Zoom from "react-reveal/Zoom";
+import Slide from "react-reveal/Slide";
+import { ExpandMoreRounded } from "@material-ui/icons";
 
 export default function About({ menuOpen, setMenuOpen }) {
   const textRef = useRef();
 
   useEffect(() => {
-    init(textRef.current, {
-      showCursor: true,
-      backDelay: 1000,
-      strings: [
-        "competitive programmer.",
-        "full stack developer.",
-        "python developer.",
-        "gnu/linux enthusiast.",
-        "content creator.",
-        "valorant player :)",
-      ],
-    });
+    setTimeout(() => {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1000,
+        strings: [
+          "competitive programmer.",
+          "full stack developer.",
+          "python developer.",
+          "gnu/linux enthusiast.",
+          "content creator.",
+          "valorant player :)",
+        ],
+      });
+    }, 1000);
   }, []);
 
   function check() {
@@ -27,16 +32,18 @@ export default function About({ menuOpen, setMenuOpen }) {
   }
   return (
     <div className="about" id="about" onClick={check}>
-      <div className="wrapper">
-        <h2>Hey there, I am</h2>
-        <h1>Yash Chauhan</h1>
-        <h3>CSE Student @ The National Institute of Engineering, Mysore</h3>
-        <h3>
-          I am a <span ref={textRef}></span>
-        </h3>
-      </div>
+      <Zoom bottom cascade>
+        <div className="wrapper">
+          <h2>Hey there, I am</h2>
+          <h1>Yash Chauhan</h1>
+          <h3>CSE Student @ The National Institute of Engineering, Mysore</h3>
+          <h3>
+            I am a <span ref={textRef}></span>
+          </h3>
+        </div>
+      </Zoom>
       <a href="#skills">
-        <img src="assets/down_arrow.png" alt="" />
+        <ExpandMoreRounded className="down-icon" />
       </a>
     </div>
   );
